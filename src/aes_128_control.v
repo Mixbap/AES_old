d99fbbf03a44c56837bd9f3dd7be373ed3b79c29
Dima Kalinin <maks-kings@yandex.ru>
2018-08-22 09:58:42 +0300

    Add blocks keyram, control and modified top

--- src/aes_128_control.v
+++ src/aes_128_control.v
@@ -1,6 +1,6 @@
 /**************************************************************************************************
  *                                                                                                *
- *  File Name:     aes_128_top.v                                                                  *
+ *  File Name:     aes_128_control.v                                                              *
  *                                                                                                *
  **************************************************************************************************
  *                                                                                                *
@@ -12,73 +12,117 @@
  *  Verilog code                                                                                  *
  **************************************************************************************************/
 
-module aes_128_top (
+module aes_128_control (
 	clk,
 	kill,
-	in_data,
 	in_en,
-	key_round,
 
+	en_mixcol,
+	rounds_end,
 	key_ready,
-	out_data,
+	idle,
 	out_en
 	);
 
 /**************************************************************************************************
 *        I/O PORTS
  **************************************************************************************************/
-input			clk;
-input			kill;
-input	[127:0]		in_data;
-input			in_en;
-input	[127:0]		key_round;
+input		clk;
+input		kill;
+input		in_en;
 
-output			key_ready;
-output	[127:0]		out_data;
-output			out_en;
+output	reg	en_mixcol;
+output	reg	rounds_end;
+output	reg	out_en;
+output		key_ready;
+output	reg	idle;
 
 /**************************************************************************************************
  *      LOCAL WIRES, REGS                                                                         *
  **************************************************************************************************/
-wire			en_mixcol;
-wire			rounds_end;
-wire			start;
-wire			idle;
+reg		in_en_r;
+reg		key_ready_r;
+reg	[4:0]	round_count;
 
 /**************************************************************************************************
  *      LOGIC                                                                                     *
  **************************************************************************************************/
-//aes_128_core
-aes_128_core aes_128_core (		.clk(clk),
-					.kill(kill),
-					.en_mixcol(en_mixcol),
-					.start(start),
-					.rounds_end(rounds_end),
-					.in_data(in_data),
-					.key_round(key_round),
-					.out_data(out_data));
+//round_count
+always @(posedge clk)
+	if (kill)
+		round_count <= 5'b0;
+	else if (in_en)
+		round_count <= 5'b0;
+	else
+		round_count <= round_count + 5'b1;
 
 /**************************************************************************************************/
-//aes_128_control
-aes_128_control aes_128_control(	.clk(clk),
-					.kill(kill),
-					.in_en(in_en),
-					.en_mixcol(en_mixcol),
-					.rounds_end(rounds_end),
-					.key_ready(key_ready),
-					.idle(idle),
-					.out_en(out_en));
+//en_mixcol
+always @(posedge clk)
+	if (kill)
+		en_mixcol <= 1'b0;
+	else if (in_en)
+		en_mixcol <= 1'b0;
+	else if (round_count == 5'd27)
+		en_mixcol <= 1'b1;
+	else 
+		en_mixcol <= 1'b0;
+		
+/**************************************************************************************************/
+//rounds_end
+always @(posedge clk)
+	if (kill)
+		rounds_end <= 1'b0;
+	else if (in_en)
+		rounds_end <= 1'b0;
+	else if (round_count == 5'd29)
+		rounds_end <= 1'b1;
+	else
+		rounds_end <= 1'b0;
 
 /**************************************************************************************************/
-//start
-assign start = (idle) ? 1'b0 : in_en;
+//key_ready_r
+always @(posedge clk)
+	if (kill)
+		key_ready_r <= 1'b0;
+	else if ((round_count == 5'd1 | 		round_count == 5'd4 | 		round_count == 5'd7 | 		round_count == 5'd10 |
+	    	  round_count == 5'd13 | 		round_count == 5'd16 | 		round_count == 5'd19 | 		round_count == 5'd22 |
+	    	  round_count == 5'd25 | 		round_count == 5'd28) & in_en_r)
+		key_ready_r <= 1'b1;
+	else
+		key_ready_r <= 1'b0;
+
+assign key_ready = in_en | key_ready_r;
 
 /**************************************************************************************************/
-endmodule
+//out_en
+always @(posedge clk)
+	if (kill)
+		out_en <= 1'b0;
+	else if (round_count == 5'd29)
+		out_en <= 1'b1;
+	else
+		out_en <= 1'b0;
 
+/**************************************************************************************************/
+//in_en_r
+always @(posedge clk)
+	if (kill)
+		in_en_r <= 1'b0;
+	else if (in_en)
+		in_en_r <= 1'b1;
+	else if (out_en)
+		in_en_r <= 1'b0;
 
+/**************************************************************************************************/
+//idle
+always @(posedge clk)
+	if (kill)
+		idle <= 1'b0;
+	else if (in_en)
+		idle <= 1'b1;
+	else if (out_en)
+		idle <= 1'b0;
 
-
-
-
-
+/**************************************************************************************************/
+endmodule;
